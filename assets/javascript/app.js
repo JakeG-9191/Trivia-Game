6b66b3bae47eddae465552c9328128870426b53b
Jake Garlick <jacobgarlick@msn.com>
2019-09-14 10:29:48 -0600

    additional introduction of js for game response

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,58 @@ var intervalId;
 var time = 150;
 setTimeout(lost, 150000);
 
+
+var questions = {
+    Q1: {
+        inqury: "how many fives is right?",
+        answer: ["1", "2", "3", "5"],
+    },
+    Q2: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q3: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q4: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q5: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q6: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q7: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q8: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q9: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+    Q10: {
+        inqury: "how would you know?",
+        answer: ["yes", "no", "i dont", "5"],
+    },
+};
+
+console.log(questions.Q1.inqury);
+console.log(questions.Q1.answer[3]);
+console.log(questions.Q2.inqury);
+console.log(questions.Q2.answer[2]);
+
 // create a page onload for questions as soon as page loads
 window.onload = function () {
-
+    $(".question").append(questions.Q1.inqury)
 }
 // create reset function if player wants to play again
 function playAgain() {
